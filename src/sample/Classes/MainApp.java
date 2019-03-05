452177896980f00840e09a3c8931558c706f2bf0
River Endless <myowngollum@gmail.com>
2019-03-05 16:03:35 +0300

    Running title added

--- src/sample/Classes/MainApp.java
+++ src/sample/Classes/MainApp.java
@@ -2,14 +2,18 @@ package sample.Classes;
 
 import formLayout.*;
 import javafx.application.Application;
-import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+
+import java.awt.*;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.CountDownLatch;
+import javafx.application.Platform;
 
 public class MainApp extends Application {
 
-    private final int WINFORM_WIDTH = 1100;
+    private final int WINFORM_WIDTH = 750;
     private final int WINFORM_HEIGHT = 300;
     private final String WINFORM_NAME = "UI design";
 
@@ -19,6 +23,8 @@ public class MainApp extends Application {
         primaryStage.setTitle(WINFORM_NAME);
         primaryStage.setWidth(WINFORM_WIDTH);
         primaryStage.setHeight(WINFORM_HEIGHT);
+        String stageTitle = primaryStage.getTitle();
+        String WHITE_SPACE = " ";
 
         FirstForm firstF = new FirstForm();
         SecondForm secondF = new SecondForm();
@@ -28,20 +34,33 @@ public class MainApp extends Application {
         MainForm mainF = new MainForm(firstF, secondF, thirdF, fourthF, fifthF);
         Scene mainScene = new Scene(mainF.getRoot_GridPane());
         primaryStage.setScene(mainScene);
-        primaryStage.show();
 
-        String title = primaryStage.getTitle();
+        Runnable task = new Runnable() {
+            public void run() {
+                new Thread(() -> {
+                    while (mainScene.getWindow().isShowing()) {
+                        Platform.runLater(() -> {
+                            primaryStage.setTitle(WHITE_SPACE + primaryStage.getTitle());
 
-        Platform.runLater( () -> {
-            while (true) {
-                primaryStage.setTitle(" " + primaryStage.getTitle());
-                try {
-                    TimeUnit.MICROSECONDS.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                            if (primaryStage.getTitle().length() >= primaryStage.getWidth() / 4) {
+                                primaryStage.setTitle(stageTitle);
+                            }
+                        });
+
+                        try {
+                            TimeUnit.MILLISECONDS.sleep(30);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }).start();
             }
-        });
+        };
+
+        new Thread(task).start();
+
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
